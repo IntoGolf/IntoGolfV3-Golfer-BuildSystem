d2b50df0265980577c5e039b5ea6f658ea0c0e2d
Laurens Luder <laurens@intogolf.nl>
2023-06-08 13:59:44 +0200

    update

--- src/config/envparser.js
+++ src/config/envparser.js
@@ -3,8 +3,10 @@ let parsedEnvCommon = DotEnv.config({ path: ".env" }).parsed;
 
 argsEnv = {};
 //if (process.env.ENVIRONMENT && process.env.ENVIRONMENT !== "live") {
-console.log("use env file: .env." + process.env.ENVIRONMENT);
-argsEnv = DotEnv.config({ path: ".env." + process.env.ENVIRONMENT }).parsed;
+if (process.env.ENVIRONMENT) {
+  console.log("use env file: .env." + process.env.ENVIRONMENT);
+  argsEnv = DotEnv.config({ path: ".env." + process.env.ENVIRONMENT }).parsed;
+}
 // } else if (process.env.ENVIRONMENT && process.env.ENVIRONMENT === "live") {
 //   console.log("use env live file: dist/.env");
 //   argsEnv = DotEnv.config({ path: "dist/.env" }).parsed;
