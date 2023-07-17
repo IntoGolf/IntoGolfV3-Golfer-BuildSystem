f20c5644d8fb8c31559de97eb258a42520d8088a
Laurens Luder <laurens@intogolf.nl>
2023-07-17 15:06:49 +0200

    update

--- src/config/envparser.js
+++ src/config/envparser.js
@@ -3,6 +3,7 @@ let parsedEnvCommon = DotEnv.config({ path: ".env" }).parsed;
 
 argsEnv = {};
 //if (process.env.ENVIRONMENT && process.env.ENVIRONMENT !== "live") {
+console.log("go johnny go");
 if (process.env.ENVIRONMENT) {
   console.log("use env file: .env." + process.env.ENVIRONMENT);
   argsEnv = DotEnv.config({ path: ".env." + process.env.ENVIRONMENT }).parsed;
