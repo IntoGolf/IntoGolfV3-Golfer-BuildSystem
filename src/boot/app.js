b12e849999aaeb62e8e788a9690874ae3269e4f8
Laurens Luder <laurens@intogolf.nl>
2025-02-18 15:02:41 +0100

    update

--- src/boot/app.js
+++ src/boot/app.js
@@ -1,4 +1,4 @@
-import { boot } from "quasar/wrappers";
+import {boot} from "quasar/wrappers";
 import dayjs from "dayjs";
 
 let customParseFormat = require("dayjs/plugin/customParseFormat");
@@ -16,35 +16,14 @@ dayjs.extend(isSameOrBefore);
 let isToday = require("dayjs/plugin/isToday");
 dayjs.extend(isToday);
 
-export default boot(({ app }) => {
+export default boot(({app}) => {
   app.config.globalProperties.$dayjs = dayjs;
 
-  // if ("serviceWorker" in navigator) {
-  //   window.addEventListener("load", () => {
-  //     navigator.serviceWorker
-  //       .register("/service-worker.js")
-  //       .then((registration) => {
-  //         registration.onupdatefound = () => {
-  //           const installingWorker = registration.installing;
-  //           installingWorker.onstatechange = () => {
-  //             if (installingWorker.state === "installed") {
-  //               if (navigator.serviceWorker.controller) {
-  //                 // Notify user of the new version
-  //                 if (
-  //                   confirm(
-  //                     "A new version is available. Would you like to update?"
-  //                   )
-  //                 ) {
-  //                   window.location.reload();
-  //                 }
-  //               }
-  //             }
-  //           };
-  //         };
-  //       })
-  //       .catch((error) => {
-  //         console.error("Service Worker registration failed:", error);
-  //       });
-  //   });
-  // }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.unregister();
+      });
+    });
+  }
 });
